40b0be48776385fb131cf605e01496cc3b3595d2
qusma <qusmablog@gmail.com>
2013-12-17 16:15:22 +0200

    Added FallBack Insturment property. In the future this will allow real time data streams to "fall back" to an alternative in case of disconnection.

--- QDMS/Requests/RealTimeDataRequest.cs
+++ QDMS/Requests/RealTimeDataRequest.cs
@@ -14,14 +14,26 @@ namespace QDMS
         [ProtoMember(1)]
         public BarSize Frequency { get; set; }
 
+        /// <summary>
+        /// Regular trading hours data only.
+        /// </summary>
         [ProtoMember(2)]
         public bool RTHOnly { get; set; }
 
         [ProtoMember(3)]
         public Instrument Instrument { get; set; }
 
+        /// <summary>
+        /// Save incoming data to local storage.
+        /// </summary>
         [ProtoMember(4)]
         public bool SaveToLocalStorage { get; set; }
+
+        /// <summary>
+        /// If the data stream for the requested instrument fails, fall back to this one instead.
+        /// </summary>
+        [ProtoMember(5)]
+        public Instrument FallBack { get; set; }
 
         public RealTimeDataRequest()
         {
