0ef17e61bccf15929db4cc7b8df79a8b8ac28121
qusma <qusmablog@gmail.com>
2013-12-24 00:44:34 +0200

    Added FoundFrontContractEventArgs class.

---
+++ QDMS/EventArguments/FoundFrontContractEventArgs.cs
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="FoundFrontContractEventArgs.cs" company="">
+// Copyright 2013 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+
+namespace QDMS
+{
+    public class FoundFrontContractEventArgs : EventArgs
+    {
+        public int ID { get; private set; }
+        public Instrument Instrument { get; private set; }
+
+        public FoundFrontContractEventArgs(int id, Instrument instrument)
+        {
+            ID = id;
+            Instrument = instrument;
+        }
+    }
+}
