9279dbede1cfad78a26f8c8652217a1a679ad796
qusma <qusmablog@gmail.com>
2013-12-26 23:54:17 +0200

    Added RealTimeDataBroker tests file.

---
+++ QDMSTest/RealTimeDataBrokerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using QDMS;
+using QDMSServer;
+
+namespace QDMSTest
+{
+    [TestFixture]
+    public class RealTimeDataBrokerTest
+    {
+        private RealTimeDataBroker _broker;
+        private Mock<IRealTimeDataSource> _ds;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _ds = new Mock<IRealTimeDataSource>();
+            _broker = new RealTimeDataBroker(5555, 5556, new List<IRealTimeDataSource> { _ds.Object });
+        }
+
+        [Test]
+        public void RequestsCorrectFuturesContractForContinuousFutures()
+        {
+            Assert.IsTrue(false);
+
+        }
+    }
+}
