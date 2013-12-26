8dacbeafaad13caee44736e400aa911ef85dcc5d
qusma <qusmablog@gmail.com>
2013-12-26 17:44:24 +0200

    Added FrontContractRequest class.

---
+++ QDMS/Requests/FrontContractRequest.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="FrontContractRequest.cs" company="">
+// Copyright 2013 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+
+namespace QDMS
+{
+    public class FrontContractRequest
+    {
+        public int ID { get; set; }
+        public Instrument Instrument { get; set; }
+        public DateTime? Date { get; set; }
+    }
+}
