c6f29fcf68065f1e4ac209bd07d6ed191e5a9e34
qusma <qusmablog@gmail.com>
2013-12-31 04:38:10 +0200

    Moved ToInt() extension method to extensions class instead of static utils class.

--- QDMS/MyExtensions.cs
+++ QDMS/MyExtensions.cs
@@ -32,6 +32,38 @@ namespace QDMS
         }
 
         /// <summary>
+        /// Convert day of week to integer, with Monday as 0.
+        /// </summary>
+        public static int ToInt(this DayOfWeek value)
+        {
+            switch (value)
+            {
+                case DayOfWeek.Sunday:
+                    return 6;
+
+                case DayOfWeek.Monday:
+                    return 0;
+
+                case DayOfWeek.Tuesday:
+                    return 1;
+
+                case DayOfWeek.Wednesday:
+                    return 2;
+
+                case DayOfWeek.Thursday:
+                    return 3;
+
+                case DayOfWeek.Friday:
+                    return 4;
+
+                case DayOfWeek.Saturday:
+                    return 5;
+
+            }
+            return 0;
+        }
+
+        /// <summary>
         /// Returns the element that occurs most frequently in this collection.
         /// </summary>
         public static T MostFrequent<T>(this IEnumerable<T> data)
--- QDMS/Utils/MyUtils.cs
+++ QDMS/Utils/MyUtils.cs
@@ -142,34 +142,6 @@ namespace QDMS
             return (long) elapsedTime.TotalSeconds;
         }
 
-        public static int ToInt(this DayOfWeek value)
-        {
-            switch (value)
-            {
-                case DayOfWeek.Sunday:
-                    return 6;
-                    
-                case DayOfWeek.Monday:
-                    return 0;
-                    
-                case DayOfWeek.Tuesday:
-                    return 1;
-                    
-                case DayOfWeek.Wednesday:
-                    return 2;
-                    
-                case DayOfWeek.Thursday:
-                    return 3;
-                    
-                case DayOfWeek.Friday:
-                    return 4;
-                    
-                case DayOfWeek.Saturday:
-                    return 5;
-                    
-            }
-            return 0;
-        }
 
         public static IEnumerable<T> GetEnumValues<T>()
         {
