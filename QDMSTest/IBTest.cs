948302c6655c9476a64cae6d4fd69329bca43cfe
qusma <qusmablog@gmail.com>
2014-01-03 04:29:40 +0200

    Added IBTest class with some scaffolding for tests.

---
+++ QDMSTest/IBTest.cs
@@ -0,0 +1,34 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IBTest.cs" company="">
+// Copyright 2014 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using NUnit.Framework;
+
+//TODO need to abstract away the ib-csharp client to be able to mock it
+
+namespace QDMSTest
+{
+    [TestFixture]
+    public class IBTest
+    {
+        [Test]
+        public void HistoricalRequestsAreSplitToRespectRequestLimits()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [Test]
+        public void RealTimeRequestsAreReSentAfterARealTimeDataPacingViolation()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [Test]
+        public void HistoricalRequestsAreCorrectlyForwardedToTheIBClient()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
