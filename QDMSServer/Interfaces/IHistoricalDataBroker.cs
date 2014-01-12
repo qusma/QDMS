e3ca54272c444d46da41c00e54e19edf3e261bfe
qusma <qusmablog@gmail.com>
2014-01-12 19:07:20 +0200

    Added IHistoricalDataBroker interface.

---
+++ QDMSServer/Interfaces/IHistoricalDataBroker.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IHistoricalDataBroker.cs" company="">
+// Copyright 2014 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using QDMS;
+
+namespace QDMSServer
+{
+    public interface IHistoricalDataBroker
+    {
+        /// <summary>
+        /// Holds the real time data sources.
+        /// </summary>
+        Dictionary<string, IHistoricalDataSource> DataSources { get; }
+
+        void RequestHistoricalData(HistoricalDataRequest request);
+        void AddData(DataAdditionRequest request);
+        List<StoredDataInfo> GetAvailableDataInfo(Instrument instrument);
+
+        event EventHandler<HistoricalDataEventArgs> HistoricalDataArrived;
+        void Dispose();
+    }
+}
