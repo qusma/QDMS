fcf1fc68b0070684d241cbebeefc4a59a4bdc04f
qusma <qusmablog@gmail.com>
2014-01-12 22:00:34 +0200

    Added InstrumentsServerTest class.

---
+++ QDMSTest/QDMSServer/InstrumentsServerTest.cs
@@ -0,0 +1,46 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="InstrumentsServerTest.cs" company="">
+// Copyright 2014 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Moq;
+using NUnit.Framework;
+using QDMSServer;
+
+namespace QDMSTest
+{
+    [TestFixture]
+    public class InstrumentsServerTest
+    {
+        private InstrumentsServer _server;
+        private QDMSClient.QDMSClient _client;
+        private Mock<IInstrumentSource> _instrumentSourceMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _instrumentSourceMock = new Mock<IInstrumentSource>();
+            _server = new InstrumentsServer(5555, _instrumentSourceMock.Object);
+
+            _server.StartServer();
+
+            _client = new QDMSClient.QDMSClient("testingclient", "127.0.0.1", 5553, 5554, 5555, 5556);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _server.StopServer();
+            _server.Dispose();
+
+            _client.Dispose();
+        }
+
+        [Test]
+        public void SearchesForInstrumentsWithTheCorrectParameters()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
