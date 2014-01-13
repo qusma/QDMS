9f54c21312eef0f607b33eeac0e14fbb59aec24e
qusma <qusmablog@gmail.com>
2014-01-13 20:17:14 +0200

    Added RTHFilter class. Still need to implement the functionality.

---
+++ QDMSServer/RTHFilter.cs
@@ -0,0 +1,23 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="RTHFilter.cs" company="">
+// Copyright 2014 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using QDMS;
+
+namespace QDMSServer
+{
+    public static class RTHFilter
+    {
+        public static List<OHLCBar> Filter(List<OHLCBar> data, List<InstrumentSession> sessions)
+        {
+            if (data == null) throw new NullReferenceException("data");
+            if (sessions == null) throw new NullReferenceException("sessions");
+
+            return data;
+        }
+    }
+}
