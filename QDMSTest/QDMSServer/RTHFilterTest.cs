3e88be5dc82462c1ffd124e50a226c61ec1a8728
qusma <qusmablog@gmail.com>
2014-01-13 20:17:23 +0200

    Added RTHFilterTest.

---
+++ QDMSTest/QDMSServer/RTHFilterTest.cs
@@ -0,0 +1,32 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="RTHFilterTest.cs" company="">
+// Copyright 2014 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using NUnit.Framework;
+
+namespace QDMSTest
+{
+    [TestFixture]
+    public class RTHFilterTest
+    {
+        [Test]
+        public void FiltersDataOutsideRTH()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [Test]
+        public void FiltersDataOutsideRTHWhenSessionSpansMultipleDays()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [Test]
+        public void FiltersDataOutsideRTHWithMultipleSessionsInOneDay()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
