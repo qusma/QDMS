3dc4c9e0a96d7c06939457aeb3f5ab05edb5ebd6
qusma <qusmablog@gmail.com>
2014-01-13 21:20:24 +0200

    Wrote SendsFrontContractRequestToCFBrokerWhenRealTimeContinuousFuturesDataIsRequested test.

--- QDMSTest/QDMSServer/RealTimeDataBrokerTest.cs
+++ QDMSTest/QDMSServer/RealTimeDataBrokerTest.cs
@@ -17,8 +17,9 @@ namespace QDMSTest
     [TestFixture]
     public class RealTimeDataBrokerTest
     {
-        private RealTimeDataBroker _broker;
+        private RealTimeDataBroker _rtBrokerMock;
         private Mock<IRealTimeDataSource> _ds;
+        private Mock<IContinuousFuturesBroker> _cfBrokerMock;
         private QDMSClient.QDMSClient _client;
 
         [SetUp]
@@ -26,10 +27,14 @@ namespace QDMSTest
         {
             _ds = new Mock<IRealTimeDataSource>();
             _ds.SetupGet(x => x.Name).Returns("MockSource");
-            _ds.SetupGet(x => x.Connected).Returns(true);
+            _ds.SetupGet(x => x.Connected).Returns(false);
 
-            _broker = new RealTimeDataBroker(5555, 5556, new List<IRealTimeDataSource> { _ds.Object });
-            _broker.StartServer();
+            _cfBrokerMock = new Mock<IContinuousFuturesBroker>();
+
+            _rtBrokerMock = new RealTimeDataBroker(5555, 5556, new List<IRealTimeDataSource> { _ds.Object }, _cfBrokerMock.Object);
+            _rtBrokerMock.StartServer();
+
+            _ds.SetupGet(x => x.Connected).Returns(true);
 
             _client = new QDMSClient.QDMSClient("testingclient", "127.0.0.1", 5556, 5555, 5554, 5553);
             _client.Connect();
@@ -41,8 +46,8 @@ namespace QDMSTest
             _client.Disconnect();
             _client.Dispose();
 
-            _broker.StopServer();
-            _broker.Dispose();
+            _rtBrokerMock.StopServer();
+            _rtBrokerMock.Dispose();
         }
 
         [Test]
@@ -74,11 +79,53 @@ namespace QDMSTest
         }
 
         [Test]
+        public void SendsFrontContractRequestToCFBrokerWhenRealTimeContinuousFuturesDataIsRequested()
+        {
+            var inst = new Instrument
+            {
+                ID = 1,
+                Symbol = "VIXCONT",
+                UnderlyingSymbol = "VIX",
+                IsContinuousFuture = true,
+                ContinuousFuture = new ContinuousFuture()
+                {
+                    UnderlyingSymbol = new UnderlyingSymbol()
+                    {
+                        ID = 1,
+                        Symbol = "VIX",
+                        Rule = new ExpirationRule()
+                    },
+                    ID = 1,
+                    Month = 1,
+                    UnderlyingSymbolID = 1
+                },
+                Datasource = new Datasource { ID = 999, Name = "MockSource" }
+            };
+
+            _cfBrokerMock
+                .Setup(x => x.RequestFrontContract(It.IsAny<Instrument>(), It.IsAny<DateTime>()))
+                .Returns(0);
+
+            var req = new RealTimeDataRequest(inst, BarSize.FiveSeconds);
+
+            _client.RequestRealTimeData(req);
+
+            Thread.Sleep(500);
+
+            _cfBrokerMock.Verify(x => x.RequestFrontContract(
+                It.Is<Instrument>(i =>
+                    i.IsContinuousFuture == true &&
+                    i.ID.HasValue && 
+                    i.ID == 1 &&
+                    i.Symbol == "VIXCONT"
+                    ),
+                It.IsAny<DateTime>()
+                ), Times.Once);
+        }
+
+        [Test]
         public void RequestsCorrectFuturesContractForContinuousFutures()
         {
-            
-
-
             Assert.IsTrue(false);
 
         }
