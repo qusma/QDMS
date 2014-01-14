f60ef8db1fa68745681a1df98f30695cf91e990f
qusma <qusmablog@gmail.com>
2014-01-14 05:32:16 +0200

    Added RealTimeDataServerTest class.

---
+++ QDMSTest/QDMSServer/RealTimeDataServerTest.cs
@@ -0,0 +1,53 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="RealTimeDataServerTest.cs" company="">
+// Copyright 2014 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Moq;
+using NUnit.Framework;
+using QDMSServer;
+
+namespace QDMSTest
+{
+    [TestFixture]
+    public class RealTimeDataServerTest
+    {
+        private RealTimeDataServer _rtServer;
+        private QDMSClient.QDMSClient _client;
+        private Mock<IHistoricalDataBroker> _brokerMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _brokerMock = new Mock<IHistoricalDataBroker>();
+
+            //also need the real time server to keep the "heartbeat" going
+            _rtServer = new RealTimeDataServer(5555, 5554);
+            _rtServer.StartServer();
+
+            _client = new QDMSClient.QDMSClient("testingclient", "127.0.0.1", 5554, 5555, 5556, 5557);
+            _client.Connect();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _rtServer.Dispose();
+            _client.Dispose();
+        }
+
+        [Test]
+        public void StartServerStartsTheServer()
+        {
+            Assert.IsTrue(_rtServer.ServerRunning);
+        }
+
+        [Test]
+        public void StopServerStopsTheServer()
+        {
+            _rtServer.StopServer();
+            Assert.IsFalse(_rtServer.ServerRunning);
+        }
+    }
+}
