d949bd270a5388f9c234716debdeea3d796eb780
qusma <qusmablog@gmail.com>
2014-01-27 21:34:57 +0200

    Started work on a new window that will allow editing of scheduled jobs.

---
+++ QDMSServer/Windows/ScheduledJobsWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SessionTemplatesWindow.xaml.cs" company="">
+// Copyright 2013 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using EntityData;
+using MahApps.Metro.Controls;
+using QDMS;
+
+namespace QDMSServer
+{
+    /// <summary>
+    /// Interaction logic for SessionTemplatesWindow.xaml
+    /// </summary>
+    public partial class ScheduledJobsWindow : MetroWindow
+    {
+
+        public ScheduledJobsWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ModifyBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
