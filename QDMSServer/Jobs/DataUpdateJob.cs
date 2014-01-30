f2fad6654d51c915f232024623350106ef85ca4c
qusma <qusmablog@gmail.com>
2014-01-30 21:28:12 +0200

    Various bug fixes and logging improvements on DataUpdateJob.

--- QDMSServer/Jobs/DataUpdateJob.cs
+++ QDMSServer/Jobs/DataUpdateJob.cs
@@ -7,6 +7,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Windows;
 using EntityData;
 using NLog;
 using QDMS;
@@ -16,6 +18,9 @@ namespace QDMSServer
 {
     public class DataUpdateJob : IJob
     {
+        private Logger _logger;
+        private string _jobName;
+
         /// <summary>
         /// Called by the <see cref="T:Quartz.IScheduler"/> when a <see cref="T:Quartz.ITrigger"/>
         ///             fires that is associated with the <see cref="T:Quartz.IJob"/>.
@@ -31,12 +36,14 @@ namespace QDMSServer
         /// <param name="context">The execution context.</param>
         public void Execute(IJobExecutionContext context)
         {
-            var logger = LogManager.GetCurrentClassLogger();
+            _logger = LogManager.GetCurrentClassLogger();
 
             JobDataMap dataMap = context.JobDetail.JobDataMap;
             var details = (DataUpdateJobDetails)dataMap["details"];
 
-            logger.Log(LogLevel.Info, string.Format("Data Update job {0} triggered.", details.Name));
+            _jobName = details.Name;
+
+            Log(LogLevel.Info, string.Format("Data Update job {0} triggered.", details.Name));
 
             //Multiple jobs may be called simultaneously, so what we do is seed the Random based on the job name
             byte[] bytes = new byte[details.Name.Length * sizeof(char)];
@@ -45,27 +52,13 @@ namespace QDMSServer
 
             List<Instrument> instruments = new List<Instrument>();
             var im = new InstrumentManager();
-            if (details.UseTag)
-            {
-                using (var dbContext = new MyDBContext())
-                {
-                    var tag = dbContext.Tags.FirstOrDefault(x => x.ID == details.TagID);
-                    if (tag == null)
-                    {
-                        logger.Log(LogLevel.Info, string.Format("Aborting data update job {0}: tag not found.", details.Name));
-                        return;
-                    }
-                    instruments = dbContext.Instruments.Where(x => x.Tags.Contains(tag)).ToList();
-                }
-            }
-            else
-            {
-                instruments = im.FindInstruments(pred: x => x.ID == details.InstrumentID).ToList();
-            }
+            instruments = details.UseTag 
+                ? im.FindInstruments(pred: x => x.Tags.Any(y => y.ID == details.TagID)) 
+                : im.FindInstruments(pred: x => x.ID == details.InstrumentID);
 
             if (instruments.Count == 0)
             {
-                logger.Log(LogLevel.Info, string.Format("Aborting data update job {0}: instrument not found.", details.Name));
+                Log(LogLevel.Error, string.Format("Aborting data update job {0}: no instruments found.", details.Name));
                 return;
             }
 
@@ -77,35 +70,69 @@ namespace QDMSServer
                 Properties.Settings.Default.instrumentServerPort,
                 Properties.Settings.Default.hDBPort))
             {
-                client.Connect();
+                //try to connect
+                try
+                {
+                    client.Connect();
+                }
+                catch (Exception ex)
+                {
+                    Log(LogLevel.Error, string.Format("Aborting data update job {0}: connection error {1}", details.Name, ex.Message));
+                    return;
+                }
 
+                //Hook up the error event, we want to log that stuff
+                client.Error += client_Error;
+
+                //What we do here: we check what we have available locally..
+                //If there is something, we send a query to grab data between the last stored time and "now"
+                //Otherwise we send a query to grab everything since 1900
                 using (var localStorage = DataStorageFactory.Get())
                 {
                     foreach (Instrument i in instruments)
                     {
                         if (!i.ID.HasValue) continue;
 
+                        DateTime startingDT = new DateTime(1900, 1, 1);
+
                         var storageInfo = localStorage.GetStorageInfo(i.ID.Value);
                         if (storageInfo.Any(x => x.Frequency == details.Frequency))
                         {
                             var relevantStorageInfo = storageInfo.First(x => x.Frequency == details.Frequency);
-                            client.RequestHistoricalData(new HistoricalDataRequest(
-                                i,
-                                details.Frequency,
-                                relevantStorageInfo.LatestDate,
-                                DateTime.Now, //TODO this should be in the instrument's timezone...
-                                forceFreshData: true,
-                                localStorageOnly: false,
-                                saveToLocalStorage: true,
-                                rthOnly: true));
+                            startingDT = relevantStorageInfo.LatestDate;
                         }
+
+                        client.RequestHistoricalData(new HistoricalDataRequest(
+                            i,
+                            details.Frequency,
+                            startingDT,
+                            DateTime.Now, //TODO this should be in the instrument's timezone...
+                            forceFreshData: true,
+                            localStorageOnly: false,
+                            saveToLocalStorage: true,
+                            rthOnly: true));
                     }
                 }
+
+                //Requests aren't send immediately so wait before killing the client to make sure the request is sent out
+                Thread.Sleep(50);
 
                 client.Disconnect();
             }
 
-            logger.Log(LogLevel.Info, string.Format("Data Update job {0} completed.", details.Name));
+            Log(LogLevel.Info, string.Format("Data Update job {0} completed.", details.Name));
+        }
+
+        void client_Error(object sender, ErrorArgs e)
+        {
+            Log(LogLevel.Error, string.Format("QDMSClient error in data update job {0}: {1}", _jobName, e.ErrorMessage));
+        }
+
+        private void Log(LogLevel level, string message)
+        {
+            if (Application.Current != null)
+                Application.Current.Dispatcher.Invoke(()
+                    => _logger.Log(level, message));
         }
     }
 }
