f804c4ec0c7485a0c7e786612159abfd0f88484e
qusma <qusmablog@gmail.com>
2014-02-13 03:48:16 +0200

    HistoryContext UTF8 fix applied to the data db as well.

--- EntityData/Migrations/DataDBContextConfiguration.cs
+++ EntityData/Migrations/DataDBContextConfiguration.cs
@@ -4,6 +4,7 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
+using System.Configuration;
 using System.Data.Entity.Migrations;
 using MySql.Data.Entity;
 using QDMS;
@@ -18,7 +19,17 @@ namespace EntityData.Migrations.DataDBContextNS
             AutomaticMigrationsEnabled = true;
             SetSqlGenerator("MySql.Data.MySqlClient", new MySqlMigrationSqlGenerator());
             //SetSqlGenerator("System.Data.SqlClient", new SqlServerMigrationSqlGenerator());
-            //TODO solution?
+
+
+            //Dirty hack. Check the database. If we're using MySql we need to use a special HistoryContext
+            //to bypass the problem of the too-long key when the default charset is UTF8.
+            string provider = ConfigurationManager.ConnectionStrings["qdmsDataEntities"].ProviderName;
+
+            if (provider == "MySql.Data.MySqlClient")
+            {
+                SetHistoryContextFactory(MySqlProviderInvariantName.ProviderName,
+                    (existingConnection, defaultSchema) => new EntityData.Migrations.MySqlHistoryContext(existingConnection, defaultSchema));
+            }
         }
 
         protected override void Seed(DataDBContext context)
