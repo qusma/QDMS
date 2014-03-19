5cc4f42e9a5ba3081bd3d5db12dc5469c2d1db73
qusma <qusmablog@gmail.com>
2014-03-19 23:30:06 +0200

    Wrote a couple of tests for Instrument.GetTZInfo().

---
+++ QDMSTest/QDMS/InstrumentTest.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="InstrumentTest.cs" company="">
+// Copyright 2014 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using NUnit.Framework;
+using QDMS;
+
+namespace QDMSTest
+{
+    [TestFixture]
+    public class InstrumentTest
+    {
+        [Test]
+        public void GetTZInfoReturnsUTCIfExchangeIsUndefined()
+        {
+            var inst = new Instrument();
+            TimeZoneInfo tz = inst.GetTZInfo();
+            Assert.AreEqual("UTC", tz.Id);
+        }
+
+        [Test]
+        public void GetTZInfoReturnsUTCIfExchangeTimezoneIsEmptyString()
+        {
+            var inst = new Instrument { Exchange = new Exchange { Timezone = "" } };
+            TimeZoneInfo tz = inst.GetTZInfo();
+            Assert.AreEqual("UTC", tz.Id);
+        }
+
+        [Test]
+        [ExpectedException]
+        public void GetTZInfoThrowsExceptionIfNonExistingTimezoneIsSpecified()
+        {
+            var inst = new Instrument { Exchange = new Exchange { Timezone = "asdf____" } };
+            TimeZoneInfo tz = inst.GetTZInfo();
+        }
+    }
+}
