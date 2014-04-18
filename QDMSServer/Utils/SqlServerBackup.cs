988323e4a3be3efbeb68863c50a03f32cdabbfca
qusma <qusmablog@gmail.com>
2014-04-18 17:57:49 +0300

    Added a class to handle SQL Server backups.

---
+++ QDMSServer/Utils/SqlServerBackup.cs
@@ -0,0 +1,52 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SqlServerBackup.cs" company="">
+// Copyright 2014 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Data.SqlClient;
+
+namespace QDMSServer
+{
+    public static class SqlServerBackup
+    {
+        public static void Backup(string connectionString, string database, string file)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                var cmd = new SqlCommand("", conn);
+                cmd.CommandText = string.Format(
+                                        @"USE {0};
+                                        GO
+                                        BACKUP DATABASE {0}
+                                        TO DISK = '{1}'
+                                           WITH FORMAT,
+                                              NAME = 'QDMS Server backup of {0}';
+                                        GO", 
+                                        database, 
+                                        file);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public static void Restore(string connectionString, string database, string file)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                var cmd = new SqlCommand("", conn);
+                cmd.CommandText = string.Format(
+                                        @"USE master
+                                        GO
+                                        RESTORE DATABASE {0}
+                                            FROM DISK = '{1}'
+                                        WITH REPLACE
+                                        GO", 
+                                        database, 
+                                        file);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
