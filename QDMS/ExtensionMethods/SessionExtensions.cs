81a4afc6c51565ff0a3141ae8563c919b01beaac
qusma <qusmablog@gmail.com>
2014-04-29 04:27:04 +0300

    Added Overlaps() session extension method.

--- QDMS/ExtensionMethods/SessionExtensions.cs
+++ QDMS/ExtensionMethods/SessionExtensions.cs
@@ -20,5 +20,75 @@ namespace QDMS
             result.IsSessionEnd = session.IsSessionEnd;
             return result;
         }
+
+        public static bool Overlaps(this ISession session1, ISession session2)
+        {
+            //session 1 engulfs session 2
+            if (session1.OpeningDay < session2.OpeningDay &&
+                session1.ClosingDay > session2.ClosingDay &&
+                session2.OpeningDay < session2.ClosingDay)
+            {
+                return true;
+            }
+
+            //session 2 engulfs session 1
+            if (session2.OpeningDay < session1.OpeningDay &&
+                session2.ClosingDay > session1.ClosingDay &&
+                session1.OpeningDay < session1.ClosingDay)
+            {
+                return true;
+            }
+
+            //session 1 engulfs session 2, across weeks
+            if (session1.OpeningDay > session1.ClosingDay &&
+                session1.OpeningDay > session2.OpeningDay &&
+                session1.ClosingDay > session2.ClosingDay)
+            {
+                return true;
+            }
+
+            //session 2 engulfs session 1, across weeks
+            if (session2.OpeningDay > session2.ClosingDay &&
+                session2.OpeningDay > session1.OpeningDay &&
+                session2.ClosingDay > session1.ClosingDay)
+            {
+                return true;
+            }
+
+            //partial overlap intraweek, 1 over 2
+            if (session1.OpeningDay < session2.ClosingDay &&
+                session1.ClosingDay > session2.ClosingDay)
+            {
+                return true;
+            }
+
+            //partial overlap intraweek, 2 over 1
+            if (session2.OpeningDay < session1.ClosingDay &&
+                session2.ClosingDay > session1.ClosingDay)
+            {
+                return true;
+            }
+
+            //same day, times overlap
+            if (session1.OpeningDay == session2.OpeningDay &&
+                session1.ClosingDay == session2.ClosingDay)
+            {
+                if (session1.OpeningTime > session2.OpeningTime && session1.ClosingTime < session2.ClosingTime)
+                    return true;
+                if (session1.OpeningTime < session2.OpeningTime && session1.ClosingTime > session2.OpeningTime)
+                    return true;
+                if (session1.OpeningTime < session2.ClosingTime && session1.ClosingTime > session2.ClosingTime)
+                    return true;
+
+                if (session2.OpeningTime > session1.OpeningTime && session2.ClosingTime < session1.ClosingTime)
+                    return true;
+                if (session2.OpeningTime < session1.OpeningTime && session2.ClosingTime > session1.OpeningTime)
+                    return true;
+                if (session2.OpeningTime < session1.ClosingTime && session2.ClosingTime > session1.ClosingTime)
+                    return true;
+            }
+
+            return false;
+        }
     }
 }
