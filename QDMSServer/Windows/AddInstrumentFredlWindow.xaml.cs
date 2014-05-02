f11ab36584d8d491b7fecbfe84a84c90cae587d4
qusma <qusmablog@gmail.com>
2014-05-02 15:24:40 +0300

    Added a new window for finding series from FRED. Not fully functional yet.

---
+++ QDMSServer/Windows/AddInstrumentFredlWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AddInstrumentFredlWindow.xaml.cs" company="">
+// Copyright 2014 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using EntityData;
+using MahApps.Metro.Controls;
+using NLog;
+using QDMS;
+
+namespace QDMSServer
+{
+    /// <summary>
+    /// Interaction logic for AddInstrumentQuandlWindow.xaml
+    /// </summary>
+    public partial class AddInstrumentFredWindow : MetroWindow
+    {
+        public ObservableCollection<FredUtils.FredSeries> Series { get; set; }
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public List<Instrument> AddedInstruments { get; set; }
+
+        private readonly Datasource _thisDS;
+        private readonly MyDBContext _context;
+
+        private string _apiKey = "f8d71bdcf1d7153e157e0baef35f67db";
+
+        public AddInstrumentFredWindow(MyDBContext context)
+        {
+            DataContext = this;
+
+            AddedInstruments = new List<Instrument>();
+
+            Series = new ObservableCollection<FredUtils.FredSeries>();
+
+            InitializeComponent();
+
+            _context = context;
+            _thisDS = _context.Datasources.First(x => x.Name == "FRED");
+
+            //TODO need some sort of upgrade procedure that adds datasources
+
+            ShowDialog();
+        }
+
+        private void Search(int page = 1)
+        {
+            Series.Clear();
+            
+            var foundSeries = FredUtils.FindSeries(SymbolTextBox.Text, _apiKey);
+            foreach (var i in foundSeries)
+            {
+                Series.Add(i);
+            }
+
+            StatusLabel.Content = foundSeries.Count() + " contracts found";
+        }
+
+        private void SymbolTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                Search();
+        }
+
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Search();
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int count = 0;
+            foreach (Instrument newInstrument in InstrumentGrid.SelectedItems)
+            {
+                if (newInstrument.Exchange != null)
+                    newInstrument.ExchangeID = newInstrument.Exchange.ID;
+                if (newInstrument.PrimaryExchange != null)
+                    newInstrument.PrimaryExchangeID = newInstrument.PrimaryExchange.ID;
+
+                try
+                {
+                    if (InstrumentManager.AddInstrument(newInstrument))
+                        count++;
+                    AddedInstruments.Add(newInstrument);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error");
+                }
+            }
+            StatusLabel.Content = string.Format("{0}/{1} instruments added.", count, InstrumentGrid.SelectedItems.Count);
+        }
+    }
+}
