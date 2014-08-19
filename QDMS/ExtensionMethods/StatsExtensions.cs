d2b4483c1599c26613c47da054f6930dd2b311fc
qusma <qusmablog@gmail.com>
2014-08-19 21:07:51 +0300

    Added StandardDeviation() extension methods.

---
+++ QDMS/ExtensionMethods/StatsExtensions.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="StatsExtensions.cs" company="">
+// Copyright 2014 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QDMS
+{
+    public static class StatsExtensions
+    {
+        public static double StandardDeviation(this IEnumerable<double> values)
+        {
+            double avg = values.Average();
+            return Math.Sqrt(values.Average(v => Math.Pow(v - avg, 2)));
+        }
+
+        public static double StandardDeviation(this IEnumerable<decimal> values)
+        {
+            decimal avg = values.Average();
+            return Math.Sqrt(values.Average(v => Math.Pow((double) (v - avg), 2)));
+        }
+    }
+}
