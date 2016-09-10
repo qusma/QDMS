06b1a2deb612167b98d8b4f90060e5642e762d42
qusma <qusmablog@gmail.com>
2016-09-11 00:07:11 +0300

    Fixed ContinuousFuture serialization bug and added a test for it

---
+++ QDMSTest/QDMS/ContinuousFutureTest.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ContinuousFutureTest.cs" company="">
+// Copyright 2016 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.IO;
+using NUnit.Framework;
+using QDMS;
+
+namespace QDMSTest
+{
+    [TestFixture]
+    public class ContinuousFutureTest
+    {
+        [Test]
+        public void UseMonthSerializesCorrectlyWhenSetToFalse()
+        {
+            var ms = new MemoryStream();
+            var cf = new ContinuousFuture();
+            cf.UseDec = false;
+            var serialized = MyUtils.ProtoBufSerialize(cf, ms);
+            var cf2 = MyUtils.ProtoBufDeserialize<ContinuousFuture>(serialized, ms);
+            Assert.AreEqual(false, cf2.UseDec);
+        }
+    }
+}
