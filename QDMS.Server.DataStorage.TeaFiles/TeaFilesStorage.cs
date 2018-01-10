17f08e4130380fa17b81301dc4225c494e1b88d3
qusma <qusmablog@gmail.com>
2018-01-10 19:35:04 +0200

    Start work on TeaFiles tick storage

---
+++ QDMS.Server.DataStorage.TeaFiles/TeaFilesStorage.cs
@@ -0,0 +1,12 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="TeaFilesStorage.cs" company="">
+// Copyright 2018 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace QDMS.Server.DataStorage
+{
+    public class TeaFilesStorage
+    {
+    }
+}
