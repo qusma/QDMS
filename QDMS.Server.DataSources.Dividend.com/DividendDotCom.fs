9fe193631decf3836bbf1af2b64b5fe376df5de6
qusma <qusmablog@gmail.com>
2020-01-26 20:39:50 +0200

    fixed dividend.com client

--- QDMS.Server.DataSources.Dividend.com/DividendDotCom.fs
+++ QDMS.Server.DataSources.Dividend.com/DividendDotCom.fs
@@ -9,128 +9,9 @@ module DivDotCom =
     open FSharp.Data;
     open System.Text.RegularExpressions
     open System.Globalization
+    open System.Net.Http.Headers
     //sometimes not entirely correct dividend values...
-    type jsonProv = FSharp.Data.JsonProvider<""" 	
-    {  
-   "total":3,
-   "rows":[  
-      {  
-         "symbol":"\u003ca href=\"/dividend-stocks/technology/semiconductor-broad-line/cy-cypress-semiconductor/\"\u003eCY\u003c/a\u003e",
-         "_symbol_data":{  
-            "th":"Stock Symbol"
-         },
-         "Yields.Stock":"\u003ca href=\"/dividend-stocks/technology/semiconductor-broad-line/cy-cypress-semiconductor/\"\u003eCypress Semiconductor\u003c/a\u003e",
-         "_Yields.Stock_data":{  
-            "th":"Company Name"
-         },
-         "dars_rating":"\u003ca href='/free-trial/' class='restricted' style='vertical-align: middle;'\u003e\u003c/a\u003e",
-         "_dars_rating_data":{  
-            "th":"DARS™ Rating"
-         },
-         "exdivdate":"\u003cspan style=\"white-space: nowrap;margin-right: 20px;\"\u003e2019-06-26\u003c/span\u003e",
-         "_exdivdate_data":{  
-            "th":"Ex-Div Date"
-         },
-         "paydate":"\u003cspan style=\"white-space: nowrap;\"\u003e2019-07-18\u003c/span\u003e",
-         "_paydate_data":{  
-            "th":"Pay Date"
-         },
-         "amount":"0.11",
-         "_amount_data":{  
-            "th":"Div Payout"
-         },
-         "cache_stocks.qualification":"Unknown",
-         "_cache_stocks.qualification_data":{  
-            "th":"Qualified Dividend?"
-         },
-         "cache_stocks.price":"$22.22",
-         "_cache_stocks.price_data":{  
-            "th":"Stock Price"
-         },
-         "cache_stocks.dividend_yield":"1.98%",
-         "_cache_stocks.dividend_yield_data":{  
-            "th":"Yield"
-         }
-      },
-      {  
-         "symbol":"\u003ca href=\"/dividend-stocks/financial/foreign-regional-banks/cib-bancolombia-sa/\"\u003e\u003cspan class=\"caps\"\u003eCIB\u003c/span\u003e\u003c/a\u003e",
-         "_symbol_data":{  
-            "th":"Stock Symbol"
-         },
-         "Yields.Stock":"\u003ca href=\"/dividend-stocks/financial/foreign-regional-banks/cib-bancolombia-sa/\"\u003eBanColombia S.A.\u003c/a\u003e",
-         "_Yields.Stock_data":{  
-            "th":"Company Name"
-         },
-         "dars_rating":"\u003ca href='/free-trial/' class='restricted' style='vertical-align: middle;'\u003e\u003c/a\u003e",
-         "_dars_rating_data":{  
-            "th":"DARS™ Rating"
-         },
-         "exdivdate":"\u003cspan style=\"white-space: nowrap;margin-right: 20px;\"\u003e2019-06-26\u003c/span\u003e",
-         "_exdivdate_data":{  
-            "th":"Ex-Div Date"
-         },
-         "paydate":"\u003cspan style=\"white-space: nowrap;\"\u003e2019-07-12\u003c/span\u003e",
-         "_paydate_data":{  
-            "th":"Pay Date"
-         },
-         "amount":"0.33",
-         "_amount_data":{  
-            "th":"Div Payout"
-         },
-         "cache_stocks.qualification":"Unknown",
-         "_cache_stocks.qualification_data":{  
-            "th":"Qualified Dividend?"
-         },
-         "cache_stocks.price":"$51.57",
-         "_cache_stocks.price_data":{  
-            "th":"Stock Price"
-         },
-         "cache_stocks.dividend_yield":"2.59%",
-         "_cache_stocks.dividend_yield_data":{  
-            "th":"Yield"
-         }
-      },
-      {  
-         "symbol":"\u003ca href=\"/dividend-stocks/financial/regional-mid-atlantic-banks/lion-fidelity-southern-corp/\"\u003e\u003cspan class=\"caps\"\u003eLION\u003c/span\u003e\u003c/a\u003e",
-         "_symbol_data":{  
-            "th":"Stock Symbol"
-         },
-         "Yields.Stock":"\u003ca href=\"/dividend-stocks/financial/regional-mid-atlantic-banks/lion-fidelity-southern-corp/\"\u003eFidelity Southern Corp\u003c/a\u003e",
-         "_Yields.Stock_data":{  
-            "th":"Company Name"
-         },
-         "dars_rating":"\u003ca href='/free-trial/' class='restricted' style='vertical-align: middle;'\u003e\u003c/a\u003e",
-         "_dars_rating_data":{  
-            "th":"DARS™ Rating"
-         },
-         "exdivdate":"\u003cspan style=\"white-space: nowrap;margin-right: 20px;\"\u003e2019-06-26\u003c/span\u003e",
-         "_exdivdate_data":{  
-            "th":"Ex-Div Date"
-         },
-         "paydate":"\u003cspan style=\"white-space: nowrap;\"\u003e2019-07-01\u003c/span\u003e",
-         "_paydate_data":{  
-            "th":"Pay Date"
-         },
-         "amount":"0.12",
-         "_amount_data":{  
-            "th":"Div Payout"
-         },
-         "cache_stocks.qualification":"Qualified",
-         "_cache_stocks.qualification_data":{  
-            "th":"Qualified Dividend?"
-         },
-         "cache_stocks.price":"$30.20",
-         "_cache_stocks.price_data":{  
-            "th":"Stock Price"
-         },
-         "cache_stocks.dividend_yield":"1.59%",
-         "_cache_stocks.dividend_yield_data":{  
-            "th":"Yield"
-         }
-      }
-   ]
-}
- """>
+    type jsonProv = FSharp.Data.JsonProvider<"./response.json">
 
     type DividendDotCom() = 
         let error = Event<EventHandler<ErrorArgs>,ErrorArgs>()
@@ -143,7 +24,9 @@ module DivDotCom =
         
         let httpClient = new HttpClient(handler)
         do
-            httpClient.DefaultRequestHeaders.Add("Referer", "http://www.dividend.com/ex-dividend-dates.php");
+            httpClient.DefaultRequestHeaders.Add("Referer", "https://www.dividend.com/ex-dividend-dates/");
+            httpClient.DefaultRequestHeaders.Add("Origin", "https://www.dividend.com")
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0")
 
         let removeTags (data : string) = Regex.Replace(data, @"<[^>]*>", String.Empty)
 
@@ -167,26 +50,29 @@ module DivDotCom =
 
         member this.parse(json : string) =
             let data = jsonProv.Parse(json)
-            (data.Total, data.Rows |> Seq.map this.parseRow)
+            (data.Meta.TotalPages, data.Data |> Seq.map this.parseRow)
                         
         member this.parseRow(row) =
             new Dividend(
-                Amount = row.Amount,
-                Symbol = (row.Symbol |> urlDecode |> removeTags),
-                PaymentDate = (row.Paydate |> urlDecode |> removeTags |> parseNullableDate),
+                Amount = row.ClosestPaymentAdjustedAmount,
+                Symbol = row.Symbol.Text,
+                PaymentDate = new Nullable<DateTime>(row.ClosestPaymentPayableDate),
                 Currency = "USD",
-                ExDate = (row.Exdivdate |> urlDecode |> removeTags |> parseDate))
+                ExDate = row.ClosestPaymentExDate)
         
-        member this.dividendSubRequest(request : DividendRequest, offset : int) = 
+        member this.dividendSubRequest(request : DividendRequest, page : int) = 
             async {
-                let url = sprintf "http://www.dividend.com/data_set/?tm=362&cond={\"by_active\":null,\"by_ex_div_date\":[\"%s\",\"%s\"],\"by_stock_type\":{\"Preferred\":\"on\",\"ADR\":\"on\",\"ETF\":\"on\",\"ETN\":\"on\",\"Fund\":\"on\",\"REIT\":\"on\",\"SeniorNotes\":\"on\",\"common_stock\":\"on\"}}&no_null_sort=true&count_by_id=" (request.FromDate.ToString("yyyy-MM-dd")) (request.ToDate.ToString("yyyy-MM-dd"))
-                logger.Info("Downloading dividends from " + url + " offset " + offset.ToString()) //TODO the problem is that the broker doesn't surface the error...
+                
+                let url = "https://www.dividend.com/api/data_set/"
+                //logger.Info("Downloading dividends from " + url + " offset " + offset.ToString()) 
 
                 let req = new HttpRequestMessage()
-                req.Method <- System.Net.Http.HttpMethod.Get
+                req.Method <- System.Net.Http.HttpMethod.Post
                 req.RequestUri <- new Uri(url)
-                req.Content <- new StringContent(sprintf "{\"limit\":11,\"offset\":%i,\"order\":\"ASC\",\"sort\":\"exdivdate\"}" offset, System.Text.Encoding.UTF8, "application/json")
-                let! httpResponse = httpClient.GetAsync(url) |> Async.AwaitTask
+                req.Headers.Accept.Clear()
+                req.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"))
+                let content = new StringContent(System.String.Format("{{\"f_22_from\":\"{1}\",\"f_22_to\":\"{2}\",\"f_6\":[],\"only\":[\"meta\",\"data\"],\"page\":{0},\"r\":\"Webpage#1280\",\"tm\":\"3-ex-div-dates\"}}", page, request.FromDate.ToString("yyyy-MM-dd"), request.ToDate.ToString("yyyy-MM-dd")) , System.Text.Encoding.UTF8, "application/json")
+                let! httpResponse = httpClient.PostAsync(url, content) |> Async.AwaitTask
                 httpResponse.EnsureSuccessStatusCode() |> ignore
                 let! json = httpResponse.Content.ReadAsStringAsync() |> Async.AwaitTask
                 return json |> this.parse
@@ -194,15 +80,15 @@ module DivDotCom =
 
         member this.getDividends(request : DividendRequest) = 
             async {
-                //do a first request which gets the total, then loop grabbing 10 items each time
+                ////there are 20 records per "page"
                 let divs = new System.Collections.Generic.List<Dividend>()
-                let! (total, rows) = this.dividendSubRequest(request, 0)
-                logger.Info(sprintf "Getting %i dividends" total)
+                let! (totalPages, rows) = this.dividendSubRequest(request, 1)
+
                 divs.AddRange(rows)
-                let mutable counter = 10
-                while counter < total do
+                let mutable counter = 2
+                while counter <= totalPages do
                     let! (_, rows) = this.dividendSubRequest(request, counter)
-                    counter <- counter + 10
+                    counter <- counter + 1
                     divs.AddRange(rows)
                     this.clearCookies
                     do! Async.Sleep(rand.Next(1050, 2000))
